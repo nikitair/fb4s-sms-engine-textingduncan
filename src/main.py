1533fea42e1cf78418031972ceac58a5ea803fd8
nkstlrv <stlrvnk@gmail.com>
2024-03-01 17:25:22 +0200

    Fix conflict

--- src/main.py
+++ src/main.py
@@ -29,7 +29,6 @@ async def startup_event():
 @app.get("/")
 async def index():
     logger.info(f"{index.__name__} -- INDEX ENDPOINT TRIGGERED")
->>>>>>>>> Temporary merge branch 2
     return {"success": True, "message": "Hello World"}
 
 
