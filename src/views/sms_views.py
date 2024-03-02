41adaa1de858349c35228c36e22b83d3b93757dc
nkstlrv <stlrvnk@gmail.com>
2024-03-02 21:57:56 +0200

    Updated sms view response

--- src/views/sms_views.py
+++ src/views/sms_views.py
@@ -2,8 +2,15 @@ from utils.fub_utils import FUB
 from logs.logging_config import logger
 
 
-def send_note_to_buyer_by_sms_view(note_id: int) -> bool:
+def send_note_to_buyer_by_sms_view(note_id: int) -> dict:
     logger.info(f"{send_note_to_buyer_by_sms_view.__name__} -- SENDING NOTE AS SMS")
+
+    result = {
+        "sms_text": None,
+        "contact_name": None,
+        "contact_phone": None,
+        "sms_sent": False
+    }
 
     fub = FUB()
 
@@ -16,12 +23,16 @@ def send_note_to_buyer_by_sms_view(note_id: int) -> bool:
         buyer_id = note_data["data"]["personId"]
         note_message = note_data["data"]["body"]
 
+        result["sms_text"] = note_message
+
         # get buyer data
         buyer_data = fub.get_buyer(buyer_id)
 
         if buyer_data["success"] == True:
 
             buyer_name = buyer_data["data"]["name"]
+            result["contact_name"] = buyer_name
+
             buyer_phones = buyer_data["data"]["phones"]
 
             # get buyer phone number
@@ -30,6 +41,9 @@ def send_note_to_buyer_by_sms_view(note_id: int) -> bool:
             logger.info(f"{send_note_to_buyer_by_sms_view.__name__} -- BUYER NAME - {buyer_name}; BUYER PHONE - {buyer_phone}")
 
             if buyer_phone:
+                result["contact_phone"] = buyer_phone
 
                 # send sms
                 ...
+
+    return result
