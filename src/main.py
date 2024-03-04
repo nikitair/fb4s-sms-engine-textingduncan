5ab0703eb8f06442254c64a1eb3cdd14109fcdeb
nkstlrv <stlrvnk@gmail.com>
2024-03-04 10:25:04 +0200

    temp saving received payloads to json

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import os
-from dotenv import load_dotenv
+import json
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from schemas.fub_webhook_schemas import EventSchema
 from logs.logging_config import logger
 from logs.logging_utils import log_server_start, log_server_stop
 from views.sms_views import send_note_to_buyer_by_sms_view
-from static import texts
 
 
 load_dotenv()
@@ -38,8 +38,19 @@ async def index():
 @app.post("/sms")
 async def sms(request: EventSchema):
 
+    data = dict(request)
+    with open("data/payloads.json", "r") as f:
+        existed_payloads: list = json.load(f)
+
+    existed_payloads.append(data)
+
+    with open("data/payloads.json", "w") as f:
+        json.dump(existed_payloads, f, indent=4)
+
     logger.info(f"{sms.__name__} -- SMS ENDPOINT TRIGGERED")
-    logger.info(f"{sms.__name__} -- RECEIVED PAYLOAD - {dict(request)}")
+    logger.info(f"{sms.__name__} -- RECEIVED PAYLOAD - {data}")
+
+
 
     note_ids = request.resourceIds
     if note_ids:
