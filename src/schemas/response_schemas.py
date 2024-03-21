562f85019dccba74dbec370f3cec85e03445290d
nkstlrv <stlrvnk@gmail.com>
2024-03-21 16:20:37 +0200

    Add response schemas

---
+++ src/schemas/response_schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List, Union, Optional
+
+
+class IndexResponseSchema(BaseModel):
+    success: bool
+    message: str
+
+
+class NoteProcessingSchema(BaseModel):
+    sms_text: Union[None, str]
+    contact_name: Union[None, str]
+    contact_phone: Union[None, str]
+    sms_sent: bool
+    assigned_team_member_id: Union[None, int]
+    sms_signature: Union[None, str]
+
+
+class FUBNoteCreatedResponseSchema(BaseModel):
+    success: bool
+    data: NoteProcessingSchema
+
+
+class SimpleResponseSchema(BaseModel):
+    success: bool
