55be26072a7077eae4a2e25e89a23508f52af565
nkstlrv <stlrvnk@gmail.com>
2024-04-09 16:41:52 +0300

    Remove old data

--- src/data/blast/templates.py
+++
@@ -1,1 +0,0 @@
-t1 = "An exciting new opportunity to BUY a Mr.Greek franchise has been emailed to your inbox.\n\nPlease check it out now!\n\nAll the best,\nFindBusinesses4Sale Team"
--- src/utils/retool_utils.py
+++ src/utils/retool_utils.py
@@ -1,17 +1,19 @@
+import os
+from dotenv import load_dotenv
 import requests
 from logs.logging_config import logger
 
+load_dotenv()
 
 class Retool:
     
     def __init__(self) -> None:
         logger.info("Retool -- CLASS INITIALIZED")
 
-
     def get_sms_template(self, campaign_special_id: int, campaign_day: int):
         logger.info(f"{self.get_sms_template.__name__} -- GETTING SMS TEMPLATE WITH campaign_special_id - {campaign_special_id}; campaign_day - {campaign_day}")
         response = requests.post(
-            url="https://api.retool.com/v1/workflows/6037d941-19f8-49d2-a152-29af3b9ea8fc/startTrigger?workflowApiKey=retool_wk_019e31ef121b4c03af2302633668c492",
+            url=os.getenv("RETOOL_GET_SMS_TEMPLATE_URL"),
             json={"campaign_special_id": campaign_special_id, "campaign_day": campaign_day}
         )
         status_code = response.status_code
